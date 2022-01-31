04b801065a2be636a7635a4aeab07cccc7076c7d
frastsytt <frastsytt@gmail.com>
2022-01-31 17:32:47 +0200

    basic use of ejs

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=> {
-	res.send('<a href="/contact">Contact Us</a><br><a href="/about">About Us</a>');
+app.get('/demo', (req, res)=> {
+	res.render('index.ejs');
 });
 
-app.get('/contact', (req, res)=> {
-	res.send('<h1>Contact Us Page</h1>');
-});
-
-app.get('/about', (req, res)=> {
-	res.send('<h1>About Us Page</h1>');
-});
-
-app.get('*', (req, res)=> {
-	res.send('404. This page does not exist. <a href ="/">Go to HomePage</a>');
+app.get('/user/:username', (req, res)=> {
+	let user = req.params.username;
+	res.render('index.ejs', {username : user});
 });
 
 app.listen(3000, ()=> {
